b0330ec061f2345cf47439ad32445c967951106a
Anastasia <138315407+Anarenaa@users.noreply.github.com>
2024-12-27 23:32:28 +0200

    .

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -5,3 +5,22 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+function preventDefault(event) {
+    event.preventDefault();
+}
+
+const path = window.location.pathname;
+
+document.querySelectorAll('.menu__link').forEach(link => {
+    const linkHref = link.getAttribute('href');
+    
+    if (path === `/${linkHref}`) {
+        link.classList.add('menu__link--active');
+        link.addEventListener('click', preventDefault);
+        link.removeAttribute('href');
+    } else {
+        link.classList.remove('menu__link--active');
+        link.removeEventListener('click', preventDefault);
+    }
+});
--- js/swiper.js
+++ js/swiper.js
@@ -25,6 +25,16 @@ sliders.forEach((el) => {
         this.el.addEventListener('mouseleave', () => {
           this.autoplay.start();
         });
+      },
+      slideChange: function () {
+        document.querySelectorAll('.swiper-slide').forEach((slide) => {
+          slide.removeAttribute('title');
+        });
+
+        const elements = document.querySelectorAll('.swiper-slide-active');
+        elements.forEach((element) => {
+          element.setAttribute('title', "Клацніть два рази для перегляду фотографії");
+        });
       }
     },
     
